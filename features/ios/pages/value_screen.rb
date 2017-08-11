b9a99c38aa7b71343448dd7aab0d2e8b805913be
bejerome <jerome.benjamin@external.produban.us>
2017-08-11 00:29:25 -0400

    Page objects have been updated

--- features/ios/pages/value_screen.rb
+++ features/ios/pages/value_screen.rb
@@ -2,20 +2,37 @@ require 'calabash-cucumber/ibase'
 
 class Select_Value_Screen < Calabash::IBase
 
-	def  Isvisible(id)
-		validate element id is visible
-
+	def  Isvisible(id_string)
+		wait_for_element_exists("label marked:'#{id_string}'")
 	end
 
 	def select_picker_value(picker_id, value)
-	.  Check that picker exists
-	.  if picker id is picker_1
-	.  set value to index 0 of picker
-	.  else  set value to index 1 of picker
-	.  click on the Set button
-	.  after picker disappears, get text value from field
-	.  add a decimal value to text
-	.  compare both values
+		wait_for_element_exists("UIPickerView id:'#{picker_id}'")
+		if picker_id.eql? 'picker_1'
+		    q = query("label text:'#{value}'")
+		    while q.empty?
+		        scroll("UIPickerView id:'#{picker_id}'",:up)
+		        sleep(1)
+		        q = query("* text:'#{value}'")
+		    end
+		    #select text from picker
+		    touch("* text:'#{value}'")
+		    # click set button
+		    touch("* marked:'set_btn'")	
+		    sleep(1)
+		    wait_for_element_exists("label marked:'number_input")
+		    input_string = query("EditText id:'number_input'",:getText)
+		    input_string.should eq  value
+
+		else
+			uia(%Q[uia.selectPickerValues('{1  "#{value}"}')])
+			# click set button
+		    touch("* marked:'set_btn'")	
+		    sleep(1)
+		    wait_for_element_exists("label marked:'number_input")
+		    input_string = query("EditText id:'number_input'",:getText)
+		    input_string.should eq  value
+		end
 
 	end
 end
